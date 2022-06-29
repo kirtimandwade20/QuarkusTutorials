992744af48b65dfc6a2abcbd34e753a1f4903648
kirti.mandwade.neosoft <kirti.mandwade.neosoft@hopscotch.in>
2022-06-29 13:42:56 +0530

    kogito with drl complete example

---
+++ kogito-quickstart/src/main/java/org/quarkus/dev/queries/LoanUnit.java
@@ -0,0 +1,31 @@
+package org.quarkus.dev.queries;
+
+
+import org.quarkus.dev.model.LoanApplication;
+import org.kie.kogito.rules.DataSource;
+import org.kie.kogito.rules.DataStore;
+import org.kie.kogito.rules.RuleUnitData;
+
+public class LoanUnit implements RuleUnitData {
+
+   private int maxAmount;
+   private DataStore<LoanApplication> loanApplications;
+
+   public LoanUnit() {
+   	this(DataSource.createStore(), 0);
+   }
+
+   public LoanUnit(DataStore<LoanApplication> loanApplications, int maxAmount) {
+   	this.loanApplications = loanApplications;
+   	this.maxAmount = maxAmount;
+   }
+
+   public DataStore<LoanApplication> getLoanApplications() { return loanApplications; }
+
+   public void setLoanApplications(DataStore<LoanApplication> loanApplications) {
+   	this.loanApplications = loanApplications;
+   }
+
+   public int getMaxAmount() { return maxAmount; }
+   public void setMaxAmount(int maxAmount) { this.maxAmount = maxAmount; }
+}
